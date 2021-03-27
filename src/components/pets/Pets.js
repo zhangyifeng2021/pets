ad940634595b8b4ef1bdfdd4f8d9e43629255d94
Zhang Matt <zhangyifeng_2020@163.com>
2021-03-27 20:25:12 +0800

    process response data for UI

--- src/components/pets/Pets.js
+++ src/components/pets/Pets.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
 import './Pets.css';
 import Cats from '../cats/Cats';
+import owners from '../../mocks/owner.json';
 
 class Pets extends Component {
+    constructor(props) {
+        super(props);
+        const cats = this.getCats(owners);
+        console.log(cats);
+    }
+    getCats(owners) {
+        let pets = { 'Male': [], 'Female': [] };
+        if (owners) {
+            owners.forEach(owner => {
+                if (owner.pets) {
+                    pets[owner.gender] = pets[owner.gender].concat(owner.pets.filter(pet => pet.type === 'Cat').map(pet => pet.name));
+                }
+            });
+        }
+        console.log(pets);
+        return pets;
+    }
     render() {
         return (<div className="pets-container">
             Pets<Cats />
